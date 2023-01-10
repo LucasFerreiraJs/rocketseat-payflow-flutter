28d9824fd3ddd34973617a52b60ad0b3b879946e
Lucas Ferreira <casferreiraa@gmail.com>
2023-01-09 23:26:45 -0300

    feature(splash-page): add images

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash-page.dart';
+import 'package:payflow/shared/themes/app-colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NLW PayFlow',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
